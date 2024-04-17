b601178a732c0879ba6ce23bbd75ce78c2286546
Vanessa Burton <vanessa.burton26@gmail.com>
2024-04-17 14:04:41 +0100

    Script to run ESC.

---
+++ code/Run_ESC.R
@@ -0,0 +1,12 @@
+
+# date: 17-04-24
+# author: VB
+# desc: script to run ESC for x native tree spp, for baseline & three climate scenarios
+
+### working dirs ----
+
+#wd <- "~/Documents/Woodland-Trust/Data-Analysis/SoWT2_Spp-uncertainty" # Mac path
+wd <- "C:/Users/vbu/OneDrive - the Woodland Trust/Data-analysis/SoWT2_Spp-uncertainty" # WT path
+dirData <-  paste0(wd,"/data-raw/")
+dirScratch <-  paste0(wd,"/data-scratch/")
+dirOut <- paste0(wd,"/data-out/")
