92df2a6c0b6d00c2c4f9f915f9089cb47869a997
Vanessa Burton <vanessa.burton26@gmail.com>
2024-05-28 16:30:40 +0100

    Script to analyse ESC results

---
+++ code/Visualise_uncertainty.R
@@ -0,0 +1,13 @@
+
+# date: 28-05-24
+# author: VB
+# desc: script to assess variability/uncertainty between ESC results for native specie
+
+### working dirs ----
+
+wd <- "C:/Users/vbu/OneDrive - the Woodland Trust/Data-analysis/SoWT2_Spp-uncertainty" # WT path
+dirData <-  paste0(wd,"/data-raw/")
+dirScratch <-  paste0(wd,"/data-scratch/")
+dirOut <- paste0(wd,"/data-out/")
+
+### read in ESC results ----
