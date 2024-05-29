551449155fa3f50eca2929abfa164cd7516b16ac
Vanessa Burton <vanessa.burton26@gmail.com>
2024-05-29 10:53:34 +0100

    Run through all plots.

--- code/Visualise_uncertainty.R
+++ code/Visualise_uncertainty.R
@@ -3,6 +3,8 @@
 # author: VB
 # desc: script to assess variability/uncertainty between ESC results for native species
 
+library(terra)
+
 ### working dirs ----
 
 wd <- "C:/Users/vbu/OneDrive - the Woodland Trust/Data-analysis/SoWT2_Spp-uncertainty" # WT path
@@ -19,14 +21,14 @@ lstTimesteps <- c("2010-2030","2020-2040","2030-2050","2040-2060","2050-2070","2
 
 for (sp in lstSpecies){
   
-  sp <- lstSpecies[12]
+  #sp <- lstSpecies[12]
   # read in suitability results for all scenarios
   print(paste0("Working on species: ", sp))
   
   # per timestep
   for (ts in lstTimesteps){
     
-    ts <- lstTimesteps[6]
+    #ts <- lstTimesteps[6]
     print(paste0("Timestep: ", ts))
     
     baseline <- rast(paste0(dirOut,"chess_",sp,"_soil_suit_1991-2011.tif"))
